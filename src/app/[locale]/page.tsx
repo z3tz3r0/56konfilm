85d879340e1681375adc6587bb2e6107caada8b3
z3tz3r0 <kittipan.wang@gmail.com>
2025-08-10 21:17:16 +0700

    first commit

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,16 @@
+import { useTranslations } from "next-intl";
+import {Link } from '@/i18n/navigation';
+
+import React from 'react'
+
+const HomePage = () => {
+    const t = useTranslations('HomePage');
+    return (
+    <div>
+        <h1>{t('title')}</h1>
+        <Link href="/about">{t("about")}</Link>
+    </div>
+  )
+}
+
+export default HomePage
