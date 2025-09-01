6597756e8cca06346be37d305d5fa4b0fef2e4c0
z3tz3r0 <kittipan.wang@gmail.com>
2025-09-01 14:52:21 +0700

    refactor(components): simplify navbar and remove i18n feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,15 @@
-import { useTranslations } from 'next-intl';
-import React from 'react';
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import Link from 'next/link';
 
 const Navbar = () => {
-  const t = useTranslations('Navbar');
-
   return (
-    <>
-      <div className="flex justify-between py-8">
-        <h1>Logo</h1>
-        <Tabs>
-          <TabsList>
-            <TabsTrigger value="production">{t('production')}</TabsTrigger>
-            <TabsTrigger value="wedding">{t('wedding')}</TabsTrigger>
-          </TabsList>
-        </Tabs>
-        <div className="flex gap-8">
-          <Link href="/about">{t('about')}</Link>
-          <Link href="/portfolio">{t('portfolio')}</Link>
-          <Link href="/services">{t('services')}</Link>
-          <Link href="/contact">{t('contact')}</Link>
-        </div>
-      </div>
-    </>
+    <header>
+      <nav className="flex justify-between gap-8 py-8">
+        <Link href="/about">about</Link>
+        <Link href="/portfolio">portfolio</Link>
+        <Link href="/services">service</Link>
+        <Link href="/contact">contact us</Link>
+      </nav>
+    </header>
   );
 };
 
---
+++ src/components/TabProvider/index.tsx
@@ -0,0 +1,25 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import Link from 'next/link';
+
+const PageSelectorProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Tabs defaultValue="production" className="container mx-auto">
+      <div className="flex justify-between">
+        <Link href="/">Logo</Link>
+        <TabsList>
+          <TabsTrigger value="production">production</TabsTrigger>
+          <TabsTrigger value="wedding">wedding</TabsTrigger>
+        </TabsList>
+        <nav className="flex justify-between gap-8">
+          <Link href="/about">about</Link>
+          <Link href="/portfolio">portfolio</Link>
+          <Link href="/services">service</Link>
+          <Link href="/contact">contact us</Link>
+        </nav>
+      </div>
+      <TabsContent value="production">{children}</TabsContent>
+    </Tabs>
+  );
+};
+
+export default PageSelectorProvider;
