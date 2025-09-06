2ed777351158fff879ff87e196dc0b3c604ea4dc
Thanawat Panpinij <thanawatpanpinij@gmail.com>
2025-09-06 12:57:22 +0700

    feat(button): adjust hover glow (no spread, more blur) for better visual comfort

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow-xs hover:shadow-[0_0_5px_3px_var(--primary)]',
+          'bg-primary text-primary-foreground shadow-xs hover:shadow-[0_0_10px_var(--primary)]',
         destructive:
           'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         secondary:
